a2120b8a9a73c3ba627a01260023cd1267ce08ff
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-10-07 15:55:44 -0700

    Fixed local host

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/RanchoStop')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27019/RanchoStop')
 client = MongoClient(host=host)
 db = client.get_default_database()
 ranchos = db.ranchos
