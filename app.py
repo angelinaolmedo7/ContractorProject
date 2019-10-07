f587b936396d89407a558c22944b533a8c42d9c9
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-10-07 14:05:48 -0700

    Users can see single Ranchos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 """RanchoStop is a store focusing on a modern rebranding of Tarantulas."""
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.RanchoStop
@@ -30,3 +31,10 @@ def listing_submit():
                }
     ranchos.insert_one(listing)
     return redirect(url_for('index'))
+
+
+@app.route('/ranchos/<rancho_id>')
+def ranchos_show(rancho_id):
+    """Show a single listing."""
+    rancho = ranchos.find_one({'_id': ObjectId(rancho_id)})
+    return render_template('ranchos_show.html', rancho=rancho)
