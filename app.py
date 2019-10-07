8364b504686c05d8eb0033db16d9a31997653ac1
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-10-07 12:01:21 -0700

    Users can see all Ranchos for sale

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 """RanchoStop is a store focusing on a modern rebranding of Tarantulas."""
 from flask import Flask, render_template, request
-import rancho
+import os
+from rancho import Rancho
+
+host = os.environ.get
 
 app = Flask(__name__)
 
@@ -8,4 +11,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('index.html')
+    ranchos = [
+        Rancho('Tarantula', 'Spider', 5),
+        Rancho('Other Tarantula', 'Also a Spider', 7)
+    ]
+    return render_template('index.html', ranchos=ranchos)
