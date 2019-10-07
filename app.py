1127f878538556cd71e3757b5c3a6bd6a6d08e12
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-10-07 12:45:13 -0700

    Users can create listings

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 """RanchoStop is a store focusing on a modern rebranding of Tarantulas."""
-from flask import Flask, render_template, request
-import os
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
-from rancho import Rancho
 
 client = MongoClient()
 db = client.RanchoStop
@@ -15,3 +13,20 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     return render_template('index.html', ranchos=ranchos.find())
+
+
+@app.route('/ranchos/new')
+def playlists_new():
+    """Create a new listing."""
+    return render_template('new_listing.html')
+
+
+@app.route('/ranchos', methods=['POST'])
+def listing_submit():
+    """Submit a new listing."""
+    listing = {'title': request.form.get('title'),
+               'species': request.form.get('species'),
+               'description': request.form.get('description')
+               }
+    ranchos.insert_one(listing)
+    return redirect(url_for('index'))
