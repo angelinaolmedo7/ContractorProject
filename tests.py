5dbc5e8899e3e6ea73950d98c8742346bd14c0ea
angelinaolmedo7 <angelinaolmedo7@gmail.com>
2019-10-10 00:39:13 -0700

    Unit tests

--- tests.py
+++ tests.py
@@ -1,18 +1,21 @@
 """RanchoStop tests."""
 from unittest import TestCase, main as unittest_main, mock
 from app import app
+from datetime import datetime
 from bson.objectid import ObjectId
 
-sample_rancho_id = ObjectId('5d55cffc4a3d4031f42827a3')
-sample_rancho = {
-    'title': 'Rancho for Sale',
-    'species': 'Cobalt Blue',
-    'description': 'Selling my 1yo Cobalt Blue female.'
+sample_listing_id = ObjectId('5d55cffc4a3d4031f42827a3')
+sample_listing = {
+    'title': 'Cool listing',
+    'description': 'Buy my Rancho.',
+    'views': 0,
+    'created_at': datetime.now(),
+    'author': 'Tahoe',
+    'user_id': ObjectId('6d55cffc4a3d4031f42827a4')
 }
 sample_form_data = {
-    'title': sample_rancho['title'],
-    'species': sample_rancho['species'],
-    'description': sample_rancho['description']
+    'title': 'new title',
+    'description': sample_listing['description']
 }
 
 
@@ -31,36 +34,48 @@ class RanchoStopTests(TestCase):
         """Test the RanchoStop homepage."""
         result = self.client.get('/')
         self.assertEqual(result.status, '200 OK')
-        self.assertIn(b'Rancho', result.data)
+        self.assertIn(b'RanchoStop', result.data)
 
-    def test_new(self):
+    def test_new_listing(self):
         """Test the new listing creation page."""
         result = self.client.get('/ranchos/new')
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'New Listing', result.data)
 
-    @mock.patch('pymongo.collection.Collection.find_one')
-    def test_show_ranchos(self, mock_find):
-        """Test showing a single listing."""
-        mock_find.return_value = sample_rancho
-
-        result = self.client.get(f'/ranchos/{sample_rancho_id}')
+    def test_user_dir(self):
+        """Test the RanchoStop user directory."""
+        result = self.client.get('/users/directory')
         self.assertEqual(result.status, '200 OK')
-        self.assertIn(b'Rancho for Sale', result.data)
+        self.assertIn(b'RanchoStop', result.data)
+
+    def test_new_user(self):
+        """Test the new user creation page."""
+        result = self.client.get('/users/new')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'New User', result.data)
+
+    @mock.patch('pymongo.collection.Collection.find_one')
+    def test_show_listing(self, mock_find):
+        """Test showing a single listing."""
+        mock_find.return_value = sample_listing
+
+        result = self.client.get(f'/ranchos/{sample_listing_id}')
+        self.assertEqual(result.status, '200 OK')
+        self.assertIn(b'Buy my Rancho.', result.data)
 
     @mock.patch('pymongo.collection.Collection.find_one')
     def test_edit_listing(self, mock_find):
         """Test editing a single listing."""
-        mock_find.return_value = sample_rancho
+        mock_find.return_value = sample_listing
 
-        result = self.client.get(f'/ranchos/{sample_rancho_id}/edit')
+        result = self.client.get(f'/ranchos/{sample_listing_id}/edit')
         self.assertEqual(result.status, '200 OK')
         self.assertIn(b'Edit Listing', result.data)
 
     @mock.patch('pymongo.collection.Collection.insert_one')
     def test_submit_listing(self, mock_insert):
         """Test submitting a new listing."""
-        result = self.client.post('/ranchos', data=sample_form_data)
+        result = self.client.post('/listings', data=sample_form_data)
 
         # After submitting, should redirect to that listing's page
         self.assertEqual(result.status, '302 FOUND')
@@ -69,21 +84,21 @@ class RanchoStopTests(TestCase):
     @mock.patch('pymongo.collection.Collection.update_one')
     def test_update_listing(self, mock_update):
         """Test updating a listing."""
-        result = self.client.post(f'/ranchos/{sample_rancho_id}',
+        result = self.client.post(f'/listings/{sample_listing_id}',
                                   data=sample_form_data)
 
         self.assertEqual(result.status, '302 FOUND')
-        mock_update.assert_called_with({'_id': sample_rancho_id},
-                                       {'$set': sample_rancho})
+        mock_update.assert_called_with({'_id': sample_listing_id},
+                                       {'$set': sample_listing})
 
     @mock.patch('pymongo.collection.Collection.delete_one')
     def test_delete_listing(self, mock_delete):
         """Test deleting a listing."""
         form_data = {'_method': 'DELETE'}
-        result = self.client.post(f'/ranchos/{sample_rancho_id}/delete',
+        result = self.client.post(f'/listings/{sample_listing_id}/delete',
                                   data=form_data)
         self.assertEqual(result.status, '302 FOUND')
-        mock_delete.assert_called_with({'_id': sample_rancho_id})
+        mock_delete.assert_called_with({'_id': sample_listing_id})
 
 
 if __name__ == '__main__':
